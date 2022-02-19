25960f61a48fe61185ec91962cd27b8b1150f2d8
Gitisfun <christianvandenputtelaar@gmail.com>
2022-02-19 22:49:08 +0100

    Initial commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -51,16 +51,6 @@ body {
   height: 100vh;
 }
 
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-
 /* _CO_ Vanaf hier */
 
 
